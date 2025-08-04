eea23f787466914a3f921325e88551d1f4a990e5
jgsuess <jgsuess@users.noreply.github.com>
2025-08-04 12:29:26 +0000

    deploy: b10d9a8cecb20165b91803f6b363103301de0984

--- docker/build-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Build FHIRFLARE-IG-Toolkit Docker image
-
-# Build the image using the Dockerfile in the docker directory
-docker build -f Dockerfile -t fhirflare-ig-toolkit:latest ..
-
-echo "Docker image built successfully"
